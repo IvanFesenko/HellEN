77523b4cd341616efe8c492e9e00176901153b6f
Bignichok <bignik545@gmail.com>
2020-06-17 21:21:38 +0300

    connection between sliders

--- src/js/script.js
+++ src/js/script.js
@@ -29,4 +29,32 @@ $(document).ready(function () {
     ],
     mobileFirst: true,
   });
+
+  $('.first-slider__item').click(function (event) {
+    $('.sliderbig').slick('goTo', 1);
+  });
+
+  $('.second-slider__item').click(function (event) {
+    $('.sliderbig').slick('goTo', 2);
+  });
+
+  $('.third-slider__item').click(function (event) {
+    $('.sliderbig').slick('goTo', 3);
+  });
+
+  $('.fourth-slider__item').click(function (event) {
+    $('.sliderbig').slick('goTo', 4);
+  });
+
+  $('.fifth-slider__item').click(function (event) {
+    $('.sliderbig').slick('goTo', 5);
+  });
+
+  $('.sixth-slider__item').click(function (event) {
+    $('.sliderbig').slick('goTo', 6);
+  });
+
+  $('.seventh-slider__item').click(function (event) {
+    $('.sliderbig').slick('goTo', 7);
+  });
 });
