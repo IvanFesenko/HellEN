a56b0648fae2b580b8a329fc323384be88a412ae
Bignichok <bignik545@gmail.com>
2020-06-17 17:50:22 +0300

    slider breakpoint 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,32 @@
 $(document).ready(function () {
   $('.slider').slick({
-    dots: true,
-    speed: 500,
+    speed: 1000,
     autoplay: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 3,
+    asNavFor: '.sliderbig',
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          slidesToShow: 7,
+        },
+      },
+    ],
+    mobileFirst: true,
+  });
+
+  $('.sliderbig').slick({
+    asNavFor: '.slider',
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: { arrows: true },
+      },
+    ],
+    mobileFirst: true,
   });
 });
