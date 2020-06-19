1345a4d4b09fc7f37e93efd844761e4b6b5e26af
Ivan Fesenko <pharmivan@gmail.com>
2020-06-19 15:07:42 +0300

    show btn fix

--- src/js/backbtn.js
+++ src/js/backbtn.js
@@ -1,18 +1,16 @@
-function backToTop() {
-  let button = $('.to-header-btn');
+$(window).scroll(function () {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $('#back2Top').fadeIn();
+  } else {
+    $('#back2Top').fadeOut();
+  }
+});
 
-  $(window).on('scroll', () => {
-    if ($(this).scrollTop() >= 100) {
-      button.fadeOut();
-    } else {
-      button.fadeIn();
-    }
+$(document).ready(function () {
+  $('#back2Top').click(function (event) {
+    event.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+    return false;
   });
-
-  button.on('click', e => {
-    e.preventDefault();
-    $('html').animate({ scrollTop: 0 }, 1000);
-  });
-}
-
-backToTop();
+});
