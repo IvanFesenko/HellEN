caa577e10ab3cc74a79d11b55fd0466b6018f50d
Bignichok <bignik545@gmail.com>
2020-06-19 15:45:46 +0300

    attempte to make read more

--- src/js/script.js
+++ src/js/script.js
@@ -65,29 +65,55 @@ $(document).ready(function () {
   });
 });
 
-const readMore = document.getElementsByClassName('more-text'),
-  dots = document.getElementsByClassName('sliderbig__item-content_dots'),
-  readMoreButton = document.getElementsByClassName(
-    'sliderbig__item-content-button',
+let readMore = document.querySelectorAll('.more-text'),
+  dots = document.querySelectorAll('.sliderbig__item-content_dots'),
+  readMoreButtons = document.querySelectorAll(
+    '.sliderbig__item-content-button',
   );
 console.log(readMore);
 console.log(dots);
-console.log(readMoreButton);
-let i = 0;
-// let indexOfButton;
+console.log(readMoreButtons);
 
-// indexOfButton =
+let i = 0;
+let button;
+
+let readMoreArr = Array.from(readMore);
+
+let dotsArr = Array.from(dots);
+
+let buttonsArr = Array.from(readMoreButtons);
+
+console.log(readMoreArr);
+console.log(dotsArr);
+console.log(buttonsArr);
+
+// for (let button of readMoreButtons) {0
+//   console.log(button);
+// }
+// let indexOfButton;
+// readMoreButtons.forEach(function (button, index, buttonArr) {
+//   indexOfButton = index;
+//   console.log(indexOfButton);
+// });
+
+// map
+// let indexOfButton = buttonsArr.map(index => {
+//   return index;
+// });
+// console.log(index);
+
+// console.log(indexOfButton);
+
 function read() {
   if (!i) {
-    readMore[2].style.display = 'inline';
-    dots[2].style.display = 'none';
-    readMoreButton[2].innerHTML = 'скрыть текст';
+    readMore[0].style.display = 'inline';
+    dots[0].style.display = 'none';
+    readMoreButtons[0].innerHTML = 'скрыть текст';
     i = 1;
   } else {
-    readMore[2].style.display = 'none';
-
-    dots[2].style.display = 'inline';
-    readMoreButton[2].innerHTML = 'Читать дальше';
+    readMore[0].style.display = 'none';
+    dots[0].style.display = 'inline';
+    readMoreButtons[0].innerHTML = 'Читать дальше';
     i = 0;
   }
 }
