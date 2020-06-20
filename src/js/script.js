f09fd6dd62f84df4dd30544b5e0609ab68d81843
Bignichok <bignik545@gmail.com>
2020-06-20 14:32:51 +0300

    percent + faster

--- src/js/script.js
+++ src/js/script.js
@@ -137,14 +137,31 @@ $(function () {
   });
 });
 
-//PRELOADER
+// PRELOADER
+const preloader = document.querySelector('#preloader'),
+  percDisplay = document.querySelector('#load-perc');
 
-document.body.onload = function () {
-  setTimeout(function () {
-    const preloader = document.querySelector('#preloader');
+let images = document.images,
+  imagesTotalCount = images.length,
+  imagesLoadedCount = 0;
 
-    if (!preloader.classList.contains('done')) {
-      preloader.classList.add('done');
-    }
-  });
-};
+for (let i = 0; i < imagesTotalCount; i++) {
+  let imageClone = new Image();
+  imageClone.onload = imageLoaded;
+  imageClone.onerror = imageLoaded;
+  imageClone.src = images[i].src;
+}
+
+function imageLoaded() {
+  imagesLoadedCount++;
+  percDisplay.innerHTML =
+    (((100 / imagesTotalCount) * imagesLoadedCount) << 0) + '%';
+
+  if (imagesLoadedCount >= imagesTotalCount) {
+    setTimeout(function () {
+      if (!preloader.classList.contains('done')) {
+        preloader.classList.add('done');
+      }
+    }, 1000);
+  }
+}
