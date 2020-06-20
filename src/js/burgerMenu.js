a56710295d9ca826fb5a95a64c54ee231a8e0b49
Ivan Fesenko <pharmivan@gmail.com>
2020-06-20 22:00:55 +0300

    anti scroll added + close menu after click

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -5,5 +5,15 @@ $(function () {
     $('.burger-wrap').toggleClass('burger--active');
     $('.navigation').toggleClass('popup-menu');
     $('body').toggleClass('lock');
+    $('body').toggleClass('overflow-hidden');
+  });
+});
+
+$(function () {
+  $('.menu__link').click(function (e) {
+    e.preventDefault();
+    $('.navigation').toggleClass('popup-menu');
+    $('.header__burger').toggleClass('burger--active');
+    $('body').toggleClass('overflow-hidden');
   });
 });
