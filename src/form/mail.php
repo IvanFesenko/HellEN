516e00a938c7ce39a93742036a0dcaac9aeaefc2
Andrii Zhyvotovskyi <zhyvotovskyiandrii@gmail.com>
2020-06-22 02:08:49 +0300

    corection with popup page

--- src/form/mail.php
+++ src/form/mail.php
@@ -36,6 +36,6 @@ $mail->AltBody = '';
 if(!$mail->send()) {
     echo 'Error';
 } else {
-    header('location: thanks.html');
+    '.js-overlay-thank-you';
 }
 ?>
--- src/js/formMail.js
+++ src/js/formMail.js
@@ -1,51 +1,36 @@
 $("#sendMail").on("click", function () {
-  var phone = $('#phone-mask').val();
   var name = $('#user-name').val();
+  var phone = $('#phone-mask').val();
 
-  if (phone == "") {
-    $("#errorMess").text("Введите телефон");
-    return false;
-  } else if (name == "") {
-    $("#errorMess").text("Введите имя");
-    return false;
-  }
-
-  $("#errorMess").text("");
 
   $.ajax({
-
     url: '../form/mail.php',
     type: 'POST',
     cache: false,
-    data: { 'phone': phone, 'name': name },
+    data: { 'name': name, 'phone': phone },
     dataType: 'html',
     beforeSend: function () {
-
       $("#sendMail").prop("disabled", true);
-
-      console.log
     },
     success: function (data) {
-      if (data == false)
-        alert("были ошибки");
-      else
-        $('.js-overlay-thank-you').fadeIn();
+      $('.js-overlay-thank-you').fadeIn();
+      // $(this).find('input').val('');
       $('#mail-form').trigger('reset');
-
-      console.log
-    }
-
-  })
-
-  $('.js-close-thank-you').click(function () { // по клику на крестик
-    $('.js-overlay-thank-you').fadeOut();
-  });
-
-  $(document).mouseup(function (e) { // по клику вне попапа
-    var popup = $('.popup');
-    if (e.target != popup[0] && popup.has(e.target).length === 0) {
-      $('.js-overlay-thank-you').fadeOut();
     }
   });
 
+
+})
+
+$('.js-close-thank-you').click(function () { // по клику на крестик
+  $('.js-overlay-thank-you').fadeOut();
 });
+
+$(document).mouseup(function (e) { // по клику вне попапа
+  var popup = $('.popup');
+  if (e.target != popup[0] && popup.has(e.target).length === 0) {
+    $('.js-overlay-thank-you').fadeOut();
+  }
+});
+
+
