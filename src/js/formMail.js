6860df71fff213e34af1d322b8ef40259a84a7f1
Andrii Zhyvotovskyi <zhyvotovskyiandrii@gmail.com>
2020-06-21 18:01:28 +0300

    delete files from form folder and conect the popup thanks

---
+++ src/js/formMail.js
@@ -0,0 +1,49 @@
+$("#sendMail").on("click", function () {
+  var phone = $('#phone-mask').val();
+  var name = $('#user-name').val();
+
+  if (phone == "") {
+    $("#errorMess").text("Введите телефон");
+    return false;
+  } else if (name == "") {
+    $("#errorMess").text("Введите имя");
+    return false;
+  }
+
+  $("#errorMess").text("");
+
+  $.ajax({
+
+    url: '../form/mail.php',
+    type: 'POST',
+    cache: false,
+    data: { 'phone': phone, 'name': name },
+    dataType: 'html',
+    beforeSend: function () {
+
+      $("#sendMail").prop("disabled", true);
+
+    },
+    success: function (data) {
+      if (data == false)
+        alert("были ошибки");
+      else
+        $('.js-overlay-thank-you').fadeIn();
+      $('#mail-form').trigger('reset');
+
+    }
+
+  })
+
+  $('.js-close-thank-you').click(function () { // по клику на крестик
+    $('.js-overlay-thank-you').fadeOut();
+  });
+
+  $(document).mouseup(function (e) { // по клику вне попапа
+    var popup = $('.popup');
+    if (e.target != popup[0] && popup.has(e.target).length === 0) {
+      $('.js-overlay-thank-you').fadeOut();
+    }
+  });
+
+});
