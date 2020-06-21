1399d509ffc267bad303334c52dd65158be542b0
Mykola-Vynnychuk <nik4779@ukr.net>
2020-06-21 09:33:41 +0300

    2

--- src/js/burgerMenu.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $('.header__burger').click(function (e) {
-    e.preventDefault();
-    $(this).toggleClass('burger--active');
-    $('.burger-wrap').toggleClass('burger--active');
-    $('.navigation').toggleClass('popup-menu');
-    $('body').toggleClass('lock');
-  });
-});
