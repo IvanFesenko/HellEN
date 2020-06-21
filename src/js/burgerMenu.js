70b99a6828a95daa4f3ec426fadedca66539751f
Maximusvin <Maximusvin@gmail.com>
2020-06-22 00:40:31 +0300

    del menu-wrap from js

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -2,7 +2,6 @@ $(function () {
   $('.header__burger').click(function (e) {
     e.preventDefault();
     $(this).toggleClass('burger--active');
-    $('.burger-wrap').toggleClass('burger--active');
     $('.navigation').toggleClass('popup-menu');
     $('body').toggleClass('overflow-hidden');
   });
