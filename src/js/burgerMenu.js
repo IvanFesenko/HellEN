426a375cc484017daa93d2a1ab120a780820aecb
Maximusvin <Maximusvin@gmail.com>
2020-06-22 13:22:26 +0300

    added burger close

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,7 +1,17 @@
 $(function () {
-  $('.header__burger').click(function (e) {
+  $('.burger--btn').click(function (e) {
     e.preventDefault();
     $(this).toggleClass('active');
+    $('.burger').toggleClass('active');
+    $('body').toggleClass('lock');
+    $('.navigation .logo').toggleClass('close');
+    $('.burger__wrap .logo').toggleClass('open');
+  });
+});
+
+$(function () {
+  $('.menu__item').click(function (e) {
+    e.preventDefault();
     $('.burger').toggleClass('active');
     $('body').toggleClass('lock');
     $('.navigation .logo').toggleClass('close');
