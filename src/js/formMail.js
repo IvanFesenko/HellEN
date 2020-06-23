af024744d4c85d20437222a557743e5ea0680058
Ivan Fesenko <pharmivan@gmail.com>
2020-06-23 19:06:49 +0300

    smoke fix

--- src/js/formMail.js
+++ src/js/formMail.js
@@ -1,65 +1,59 @@
-
-$("#sendMail").on("click", function () {
+$('#sendMail').on('click', function () {
   var name = $('#user-name').val();
   var phone = $('#phone-mask').val();
 
   if (name.length < 2) {
-    smoke.prompt('введите имя');
+    smoke.alert('введите имя');
     return false;
   } else if (phone.length < 18) {
-    smoke.prompt('введите номер телефона');
+    smoke.alert('введите номер телефона');
     return false;
   }
-
 
   $.ajax({
     url: '../form/mail.php',
     type: 'POST',
     cache: false,
-    data: { 'name': name, 'phone': phone },
+    data: { name: name, phone: phone },
     dataType: 'html',
     beforeSend: function () {
-      $("#sendMail").prop("disabled", true);
+      $('#sendMail').prop('disabled', true);
     },
     success: function (data) {
       $('.js-overlay-thank-you').fadeIn();
       // $(this).find('input').val('');
-      $("#sendMail").prop("disabled", false);
+      $('#sendMail').prop('disabled', false);
       $('#mail-form').trigger('reset');
-
-    }
+    },
   });
 
   $.ajax({
     url: '../form/telegram.php',
     type: 'POST',
     cache: false,
-    data: { 'name': name, 'phone': phone },
+    data: { name: name, phone: phone },
     dataType: 'html',
     beforeSend: function () {
-      $("#sendMail").prop("disabled", true);
+      $('#sendMail').prop('disabled', true);
     },
 
     success: function (data) {
       $('.js-overlay-thank-you').fadeIn();
       // $(this).find('input').val('');
-      $("#sendMail").prop("disabled", false);
+      $('#sendMail').prop('disabled', false);
       $('#mail-form').trigger('reset');
-
-    }
+    },
   });
-
-
-})
-
-$('.js-close-thank-you').click(function () { // по клику на крестик
-  $('.js-overlay-thank-you').fadeOut();
-  // location.reload();
-
-
 });
 
-$(document).mouseup(function (e) { // по клику вне попапа
+$('.js-close-thank-you').click(function () {
+  // по клику на крестик
+  $('.js-overlay-thank-you').fadeOut();
+  // location.reload();
+});
+
+$(document).mouseup(function (e) {
+  // по клику вне попапа
   var popup = $('.popup');
   if (e.target != popup[0] && popup.has(e.target).length === 0) {
     $('.js-overlay-thank-you').fadeOut();
