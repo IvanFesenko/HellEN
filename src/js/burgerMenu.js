908b195152ba5547beb49a73d957ee8c13b17df3
Ivan Fesenko <pharmivan@gmail.com>
2020-06-23 23:51:27 +0300

    one more bug fix

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -12,9 +12,10 @@ $(function () {
 $(function () {
   $('.menu__link').click(function (e) {
     e.preventDefault();
-    $('.burger').toggleClass('active');
-    $('.burger--btn').toggleClass('active');
     if (document.querySelectorAll('.lock').length > 0) {
+      $('.burger').toggleClass('active');
+      $('.burger--btn').toggleClass('active');
+
       $('body').toggleClass('lock');
       $('.navigation .logo').toggleClass('close');
       $('.burger__wrap .logo').toggleClass('open');
